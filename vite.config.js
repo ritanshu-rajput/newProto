0a715689dc7907021a86cd45510ad49ffc21fced
ritanshu-rajput <ritanshukumarsingh8922@gmail.com>
2025-07-18 01:10:47 +0530

    build updated

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,10 @@ import netlify from "@netlify/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), netlify()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
 })
